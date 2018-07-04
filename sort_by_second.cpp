23386f0d50b52add5882182a3313761ce3cedb13
Subhadeep Deyashi <36545449+subhadeep-deyashi@users.noreply.github.com>
2018-07-04 21:41:52 +0530

    Add files via upload

---
+++ sort_by_second.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool sortbysec(const pair<long int,string> &a,
+              const pair<long int,string> &b)
+{
+    return (a.second < b.second);
+}
